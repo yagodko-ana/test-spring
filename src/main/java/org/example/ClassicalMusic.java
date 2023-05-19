9483a1c711e67a44ef1bd04239a73f8f9c4dc005
ÿagodko-ana <anastasia.yagodko@gmail.com>
2023-05-19 18:12:51 +0300

    Bean-Lifecycle Init-Dest singel/proto

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package org.example;
 
 public class ClassicalMusic implements Music{
+    public void doMyInit(){
+        System.out.println("Doing my initialization (singleton)");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction (singleton)");
+    }
+
     @Override
     public String getSong() {
         return "Hun Rhapsody";
--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -1,6 +1,14 @@
 package org.example;
 
 public class RockMusic implements Music{
+    public void doMyInit(){
+        System.out.println("Doing my initialization (prototype)");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction (prototype)");
+    }
+
     @Override
     public String getSong() {
         return "Wind cries Mary";
