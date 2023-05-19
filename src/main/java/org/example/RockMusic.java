7956351b4db1cf5a1bdf725c0ed910060e9ff8c5
ÿagodko-ana <anastasia.yagodko@gmail.com>
2023-05-19 18:46:03 +0300

    CRT-with-Annotation (done start state of programm)

--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -1,13 +1,6 @@
 package org.example;
 
 public class RockMusic implements Music{
-    public void doMyInit(){
-        System.out.println("Doing my initialization (prototype)");
-    }
-
-    public void doMyDestroy(){
-        System.out.println("Doing my destruction (prototype)");
-    }
 
     @Override
     public String getSong() {
